f34a8aee55a6bcce4f9e31f6a77fe4356e3fb9c5
Max Zhao <alcasa.mz@gmail.com>
2020-05-27 11:18:53 +0200

    feat: Version filtering in dumping script

--- scripts/dump_logs.py
+++ scripts/dump_logs.py
@@ -8,16 +8,28 @@ from argmagic import argmagic
 import faastermetrics as fm
 
 
-def dump_logs(logdir: pathlib.Path, outdir: pathlib.Path):
+def dump_logs(logdir: pathlib.Path, outdir: pathlib.Path, version: str = None):
     """Output logs to the given destination directory.
 
     Args:
         logdir: Directory containing raw log entries.
         outdir: Destination for outputting collected log json.
+        version: Version requirement for inclusion.
     """
-    log_entries = [e for p in logdir.iterdir() for e in fm.parse_logfile(p)]
+    log_entries = fm.parse_logdir(logdir)
+    print(f"Loading {len(log_entries)} entries from {logdir}")
+
+    if version is not None:
+        print(f"Filtering: version={version}")
+        log_entries = filter(lambda e: e.data["version"] == version, log_entries)
+
+    log_entries = list(log_entries)
+
+
     if outdir.is_dir():
         outdir = outdir / f"{logdir.name}.json"
+
+    print(f"Dumping {len(log_entries)} entries to {outdir}")
     with open(outdir, "w") as jsfile:
         json.dump(log_entries, jsfile)
 
