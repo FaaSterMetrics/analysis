0a89a83caf754f414884f68509ff9ae6c637f4cd
Max Zhao <alcasa.mz@gmail.com>
2020-06-03 00:19:29 +0200

    feat: Add option to filter log events based on time when dumping.

--- scripts/dump_logs.py
+++ scripts/dump_logs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import pathlib
+import datetime
 from collections import Counter
 
 from argmagic import argmagic
@@ -8,23 +9,49 @@ from argmagic import argmagic
 import faastermetrics as fm
 
 
-def dump_logs(logdir: pathlib.Path, outdir: pathlib.Path, version: str = None):
+def parse_timewindow(timewindow: str) -> datetime.timedelta:
+    tdelta_args = {}
+    if timewindow.endswith("s"):
+        tdelta_args["seconds"] = int(timewindow[:-1])
+    elif timewindow.endswith("m"):
+        tdelta_args["minutes"] = int(timewindow[:-1])
+    elif timewindow.endswith("h"):
+        tdelta_args["hours"] = int(timewindow[:-1])
+    elif timewindow.endswith("d"):
+        tdelta_args["days"] = int(timewindow[:-1])
+    else:
+        raise ValueError(f"Time window should be number with d/h/m/s suffix (eg 30h) but got: {timewindow}")
+    return datetime.timedelta(**tdelta_args)
+
+
+def dump_logs(logdir: pathlib.Path, outdir: pathlib.Path, version: str = None, timewindow: str = None):
     """Output logs to the given destination directory.
 
     Args:
         logdir: Directory containing raw log entries.
         outdir: Destination for outputting collected log json.
         version: Version requirement for inclusion.
+        timewindow: Only include events inside a timewindow up to latest.
     """
     log_entries = fm.parse_logdir(logdir)
     print(f"Loading {len(log_entries)} entries from {logdir}")
 
     if version is not None:
         print(f"Filtering: version={version}")
-        log_entries = filter(lambda e: e.data["version"] == version, log_entries)
+        num_before = len(log_entries)
+        log_entries = list(filter(lambda e: e.data["version"] == version, log_entries))
+        num_after = len(log_entries)
+        print(f"  Kept {num_after}/{num_before} ({num_before - num_after} removed)")
 
-    log_entries = list(log_entries)
-
+    if timewindow is not None:
+        end_time = max(*[e.timestamp for e in log_entries])
+        timedelta = parse_timewindow(timewindow)
+        start_time = end_time - timedelta
+        print(f"Filtering timewindow of {timewindow}: {start_time} {end_time}")
+        num_before = len(log_entries)
+        log_entries = [e for e in log_entries if e.timestamp >= start_time]
+        num_after = len(log_entries)
+        print(f"  Kept {num_after}/{num_before} ({num_before - num_after} removed)")
 
     if outdir.is_dir():
         outdir = outdir / f"{logdir.name}.json"
