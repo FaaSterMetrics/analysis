e1a456e3d7d75e46f653496e0899aa223a0e40ac
Luk Burchard <luk.burchard@gmail.com>
2020-06-10 14:44:58 +0200

    Linting

--- scripts/export.py
+++ scripts/export.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import pathlib
-import pandas as pd
-
-from argmagic import argmagic
 from dataclasses import asdict
+
+import pandas as pd
+from argmagic import argmagic
 
 import faastermetrics as fm
 
-exporters = {
+EXPORTERS = {
     ".csv": lambda frame, target: frame.to_csv(target),
     ".xlsx": lambda frame, target: frame.to_xlsx(target)
 }
@@ -20,12 +20,12 @@ def main(input_data: pathlib.Path, out_name: pathlib.Path):
         input_data: File containing raw log entries.
         out_name: Destination path for the export, currently supported extensions are [.cvs, .xlsx]
     """
-    if out_name.suffix not in exporters:
+    if out_name.suffix not in EXPORTERS:
         print(f"Unknown extension {out_name.suffix}")
         return
     data = fm.load_logs(input_data)
     df = pd.json_normalize(map(asdict, data))
-    exporters[out_name.suffix](df, out_name)
+    EXPORTERS[out_name.suffix](df, out_name)
 
 
 if __name__ == "__main__":
